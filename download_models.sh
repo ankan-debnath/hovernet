85379f89c01ca831a56ef71f14fb47f987438111
ankan-debnath <ankan.s.debnath@gmail.com>
2025-06-10 05:19:29 +0000

    added download_model.sh file for model downloading

---
+++ download_models.sh
@@ -0,0 +1,3 @@
+# download_models.sh
+wget https://drive.google.com/file/d/1SbSArI3KOOWHxRlxnjchO7_MbWzB4lNR/view?usp=drive_link -O pretrained/hovernet_fast_pannuke_type_tf2pytorch.tar
+wget https://drive.google.com/file/d/1KntZge40tAHgyXmHYVqZZ5d2p_4Qr2l5/view?usp=sharing -O pretrained/ImageNet-ResNet50-Preact_pytorch.tar
